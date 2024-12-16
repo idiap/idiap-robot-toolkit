dddff3b1ec362f209fb3257045497af18dff09f9
Olivier Canévet <olivier.canevet@idiap.ch>
2024-12-16 10:11:46 +0100

    feat: Add iqi package

---
+++ iqi/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: Copyright 2024 Idiap Research Institute <contact@idiap.ch>
+# SPDX-FileContributor: Olivier Canévet <olivier.canevet@idiap.ch>
+# SPDX-License-Identifier: UNLICENSED
+#
+# This file is part of the iqi package
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("iqi")
