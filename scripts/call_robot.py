4b253d1c6e1d609aa78d5cf7e6e4397cf9259746
Olivier Canévet <olivier.canevet@idiap.ch>
2025-02-03 10:22:42 +0100

    feat: Add script to call robot functions from command line

---
+++ scripts/call_robot.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: Copyright 2025 Idiap Research Institute <contact@idiap.ch>
+# SPDX-FileContributor: Olivier Canévet <olivier.canevet@idiap.ch>
+# SPDX-License-Identifier: See LICENSE
+#
+# This file is part of the irt package
+
+import argparse
+import time
+
+import cv2
+
+from loguru import logger
+
+import irt
+
+
+if __name__ == "__main__":
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    irt.robot.add_parser_options(parser)
+    parser.add_argument(
+        "--func", type=str, required=True,
+        help="What function to call"
+    )
+    parser.add_argument(
+        "--arguments", type=str, default=None,
+        help="Function arguments to function to call"
+    )
+    args = parser.parse_args()
+
+    robot = irt.robot.build_robot_from_args(args)
+
+    if args.func == "wakeup":
+        robot.wake_up()
+
+    elif args.func == "rest":
+        robot.rest()
+
+    elif args.func == "say" and args.arguments is not None:
+        robot.say(args.arguments)
+
+    else:
+        logger.error(f"Unknown function '{args.func}'")
