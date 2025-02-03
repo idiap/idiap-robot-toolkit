3cb2141f8263589085b8e8767281712944dfbf41
Olivier Canévet <olivier.canevet@idiap.ch>
2025-02-03 10:21:35 +0100

    chore: Update year date in SPDX headers

--- scripts/display_camera.py
+++ scripts/display_camera.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-# SPDX-FileCopyrightText: Copyright 2024 Idiap Research Institute <contact@idiap.ch>
+# SPDX-FileCopyrightText: Copyright 2024-2025 Idiap Research Institute <contact@idiap.ch>
 # SPDX-FileContributor: Olivier Canévet <olivier.canevet@idiap.ch>
 # SPDX-License-Identifier: See LICENSE
 #
