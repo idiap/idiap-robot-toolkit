2db3fc1bbf52f999901976f06c1f47156e9a8d26
Olivier Canévet <olivier.canevet@idiap.ch>
2025-03-12 09:02:35 +0100

    chore: Update header

---
+++ irt/scripts/__init__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: Copyright 2025 Idiap Research Institute <contact@idiap.ch>
+# SPDX-FileContributor: Olivier Canévet <olivier.canevet@idiap.ch>
+# SPDX-License-Identifier: See LICENSE
+#
+# This file is part of the irt package
