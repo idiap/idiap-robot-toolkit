ab09ae6139f8aa6e208aed562ca72a4b7702bc03
Olivier Canévet <olivier.canevet@idiap.ch>
2025-09-23 09:43:37 +0200

    feat: Add script to call robot function from the cmdline

---
+++ irt/scripts/run_robot_func.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: Copyright 2025 Idiap Research Institute <contact@idiap.ch>
+# SPDX-FileContributor: Olivier Canévet <olivier.canevet@idiap.ch>
+# SPDX-License-Identifier: See LICENSE file
+
+import argparse
+import pathlib
+
+import irt
+
+
+def main():
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    irt.robot.add_parser_options(parser, default_robot="pepper")
+    parser.add_argument(
+        "--output-dir", type=str, default="rm-output",
+        help="Path of working directory"
+    )
+    parser.add_argument(
+        "-f", "--func", type=str, required=True,
+        help="Function to call"
+    )
+    parser.add_argument(
+        "-k", "--kwargs", type=str,
+        help="Keywords arguments"
+    )
+    # fmt: on
+    args = parser.parse_args()
+
+    output_dir = args.output_dir
+    pathlib.Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+    robot = irt.robot.build_robot_from_args(args)
+
+    func = args.func
+    if hasattr(robot, func):
+        func = getattr(robot, func)
+        func()
+
+
+if __name__ == "__main__":
+    main()
